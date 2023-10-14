89890ed0892bda1c96fb3ac1c09d569f00114b31
Azimbek <azimbekshaymanov@gmail.com>
2023-10-15 02:07:25 +0500

    initial commit one

---
+++ src/main/java/com/example/payme_project_g27/PaymeProjectG27Application.java
@@ -0,0 +1,13 @@
+package com.example.payme_project_g27;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymeProjectG27Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaymeProjectG27Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/payme_project_g27/PaymeProjectG27ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.payme_project_g27;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymeProjectG27ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
